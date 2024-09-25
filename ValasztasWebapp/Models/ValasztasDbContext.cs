064c97383fb226a26b0a9f58dcb35e56fefe1dc7
Sushkoo <szilagyidominik005@gmail.com>
2024-09-25 11:05:45 +0200

    Add project files.

---
+++ ValasztasWebapp/Models/ValasztasDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ValasztasWebapp.Models
+{
+    public class ValasztasDbContext : DbContext
+    {
+        public ValasztasDbContext(DbContextOptions<ValasztasDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Jelolt> Jeloltek {  get; set; }
+        public DbSet<Part> Partok { get; set; }
+    }
+}
